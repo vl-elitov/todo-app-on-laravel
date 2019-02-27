e01240d04c324e3549c25884a810bc7e1fbb97cc
Vladimir Anufriev <anufrievv.vvladimir@gmail.com>
2019-02-27 17:00:17 +0300

    Init project and set base settings.

---
+++ database/migrations/2019_02_27_134230_create_todos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTodosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('todos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->string('text');
+            $table->mediumText('body');
+            $table->string('due');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('todos');
+    }
+}
